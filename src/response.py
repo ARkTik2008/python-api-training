b8af7cfd4f2001e828e1f280917d442b36b832c4
Nikolay Malov <n.malov92@gmail.com>
2020-02-16 16:40:44 +0300

    Add logging timecode=01:45:14

--- src/response.py
+++ src/response.py
@@ -1,11 +1,14 @@
-
+import logging
 
 class AssertableResponse(object):
 
     def __init__(self, response):
+        logging.info("Request url={} body={}".format(response.request.url, response.request.body))
+        logging.info("Request status_code={} body={}".format(response.status_code, response.text))
         self._response = response
 
     def status_code(self, code):
+        logging.info(f"Status code should be {code}")
         return self._response.status_code == code
 
     def field(self, name):
